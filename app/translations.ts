1ea88e604a922116d3d8a72ec576823c4fd1e905
KevinBulme <kevin.bulme@gmail.com>
2016-07-28 23:29:54 +0200

    [REFACTOR](Translations)Merging funny and serious

---
+++ app/translations.ts
@@ -0,0 +1,18 @@
+import { Translator, Type } from './translator';
+
+const TRANSLATORS: Translator[] = [
+    { english: "spoiler", french: "divulgachage", type: Type.Serious, author: "Anonyme"},
+    { english: "stalk", french: "espiotraquer", type: Type.Serious, author: "Anonyme"},
+    { english: "smartphone", french: "ordiphone", type: Type.Serious, author: "Anonyme"},
+    { english: "secured shell", french: "coquillage sécurisé", type: Type.Funny, author: "Anonyme"},
+    { english: "fanzone", french: "zone des ventilateurs", type: Type.Funny, author: "Anonyme"}
+];
+
+export class Translations {
+
+    public translators: Translator[];
+
+    constructor(){
+        this.translators = TRANSLATORS ;
+    }
+}
