a81633febf5a53bf63617a0db5945c687de6582e
KevinBulme <kevin.bulme@gmail.com>
2016-07-28 18:55:50 +0200

    [REFACTOR](App)Separating assets from ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    styleUrls: ['assets/styles/app.component.css'],
+    templateUrl: 'assets/templates/app.component.html'
 })
 export class AppComponent { }
